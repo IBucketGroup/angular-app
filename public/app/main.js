af274cf7b7934c1eec4180acfa93e0992950a975
Angel S. Moreno <angelxmoreno@gmail.com>
2016-10-02 00:07:37 -0400

    Integrated ngToast notifications

--- public/app/main.js
+++ public/app/main.js
@@ -2,7 +2,15 @@
     'use strict';
 
     angular.module('IBucketGroup', [
+        'ngSanitize',
+        'ngToast',
         'app.router',
         'firebase'
-    ]);
+    ]).config(['ngToastProvider', function (ngToastProvider) {
+        ngToastProvider.configure({
+            animation: 'slide',
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+        });
+    }]);
 })();
